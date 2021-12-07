18e676f401ab04944b891029624b0e37e386ec94
ViniciusCostaFerreira <93293950+ViniciusCostaFerreira@users.noreply.github.com>
2021-12-07 16:55:45 -0300

    Add files via upload

---
+++ G.js
@@ -0,0 +1,42 @@
+let G1 = parseInt(prompt("Sua idade:"))
+let texto = ""
+
+while (G1 <= 12) {alert("Sai da daqui logo seu kid!!")
+alert("to avisando sai logo")
+alert("não to bricando")
+alert("Vai logo!")
+alert("....")
+alert("...........")
+alert("........")
+alert(".........")
+alert("Porque você ainda tá aqui?")
+alert("tão Insistente...")
+alert("Tá você ganhou")
+alert(".....")
+alert("Só que não otario")
+alert("Vai jogar mine!!!")
+alert("Mano, é serio, não tem o que fazer não?")
+alert("Sai kid")
+alert("meio metro...")
+alert("Meio chato isso não?...")
+alert("Enão sua crinaça...")
+    
+
+}
+ if (G1 === 24) {
+    alert("Hum Granola, tu é ne?")
+    
+}
+
+else if (G1 <= 17) {
+    alert("Você não deveria estar aqui, mas a vida é sua problerma é teu")
+    
+}
+else if (G1 >= 130) {
+    alert("Ta pega tá só a caveira, em? kkkk...")
+    
+}
+
+else{
+    alert("Bem-vindo")
+}
